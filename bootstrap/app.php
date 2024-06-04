eeb8d6de37b2c72ad954a5e6e051fba2d2f0a7dc
ali alavi <ali.masoudi.alavi@gmail.com>
2024-06-04 19:41:30 +0330

    remove addition inertia middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,11 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        //
-        $middleware->web(append: [
-            HandleInertiaRequests::class,
-        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
