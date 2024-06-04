d05bcf8b2249f042fd5b822ed18ccd92f692a373
ali alavi <ali.masoudi.alavi@gmail.com>
2024-06-04 19:55:49 +0330

    edit register.vue

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -93,11 +93,11 @@ const submit = () => {
                     :href="route('login')"
                     class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                 >
-                    Already registered?
+                    پیش از این ثبت نام کرده اید?
                 </Link>
 
                 <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Register
+                    ثبت نام
                 </PrimaryButton>
             </div>
         </form>
