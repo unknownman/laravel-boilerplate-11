f9683e1adf5844cbc29bb599bdf8a2ba47194b69
ali alavi <ali.masoudi.alavi@gmail.com>
2024-11-10 20:01:39 +0330

    make the visibility of new category form better

--- resources/js/Components/Ui/Category.vue
+++ resources/js/Components/Ui/Category.vue
@@ -1,29 +1,38 @@
 <template>
-    <div>
-        <button v-if="!isOpenNewCategory" @click="openNewCategory">دسته جدید</button>
-        <ui-input v-model="form.name" v-if="isOpenNewCategory" />
-        <ui-category-item :category="category" v-for="category in categories" />
+  <div>
+    <button v-if="!isOpenNewCategory" @click="openNewCategory">دسته جدید</button>
+    <div v-if="isOpenNewCategory" class="grid grid-cols-2 gap-4">
+      <ui-input v-model="form.name" />
+      <button @click="submitForm">اضافه کن</button>
     </div>
+    <ui-category-item :category="category" v-for="category in categories" />
+  </div>
 </template>
 
 <script setup>
-import { useForm } from '@inertiajs/vue3';
+import { useForm } from "@inertiajs/vue3";
 
 const props = defineProps({
-    categories: Array
-})
+  categories: Array,
+});
 
-const form=useForm({
-    name: ""
-})
+const form = useForm({
+  name: "",
+});
+const isOpenNewCategory = ref(false);
 
-const isOpenNewCategory = ref(false)
-function openNewCategory() {
-    isOpenNewCategory.value = true
+function submitForm() {
+  form.post("/categories/store", {
+    onFinish: () => {
+      form.reset();
+      isOpenNewCategory.value = false;
+    },
+  });
 }
 
+function openNewCategory() {
+  isOpenNewCategory.value = true;
+}
 </script>
 
-<style  scoped>
-
-</style>
+<style scoped></style>
