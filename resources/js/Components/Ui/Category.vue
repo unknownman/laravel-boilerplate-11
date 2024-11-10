f9ad5e0032fa823368067e16b540b01e99128919
ali alavi <ali.masoudi.alavi@gmail.com>
2024-11-10 19:27:36 +0330

    using ui-category-item to show each category in tree structure

---
+++ resources/js/Components/Ui/Category.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <ui-category-item :category="category" v-for="category in categories" />
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    categories: Array
+})
+</script>
+
+<style  scoped>
+
+</style>
