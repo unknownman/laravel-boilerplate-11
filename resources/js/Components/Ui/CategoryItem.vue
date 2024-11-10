0f69f98a4a9d92ab13f9460ceafc28ae56635871
ali alavi <ali.masoudi.alavi@gmail.com>
2024-11-10 19:42:00 +0330

    making category item to show sub categories togglable

---
+++ resources/js/Components/Ui/CategoryItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="toggle" v-if="category.children">
+      <span v-if="isOpen">-</span>
+      <span v-else>+</span>
+    </button>
+    <span :class="{ 'font-bold': isOpen }">{{ category.name }}</span>
+  </div>
+  <div class="ps-5" v-if="isOpen">
+    <ui-category :categories="category.children" />
+  </div>
+</template>
+
+<script setup>
+import { useToggle } from '@vueuse/core';
+
+
+const props = defineProps({
+  category: Object,
+});
+
+const toggle=useToggle(isOpen)
+const isOpen = ref(false);
+</script>
+
+<style scoped></style>
