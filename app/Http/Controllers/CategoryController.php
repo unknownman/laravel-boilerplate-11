dd7186a42d42c990a82435d74d33aef6949784f1
ali alavi <ali.masoudi.alavi@gmail.com>
2024-11-10 19:21:53 +0330

    making index action of category controller to fetch all categories with children

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CategoryController extends Controller
 {
     //
+    public function index() {
+
+        $categories = Category::with(['children' => function($query) {
+            $query->with("children");
+        }])->whereNull('parent_id')->get();
+
+        return Inertia::render("Categories/Index", compact("categories"));
+    }
 }
