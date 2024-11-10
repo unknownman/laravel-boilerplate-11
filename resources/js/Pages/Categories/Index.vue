6b29e615618c88dd86e275fcc4574668c11120bc
ali alavi <ali.masoudi.alavi@gmail.com>
2024-11-10 19:24:13 +0330

    making default vue 3 file for categories/index

---
+++ resources/js/Pages/Categories/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+
+defineOptions({
+    layout: AuthenticatedLayout
+})
+const props = defineProps({
+    categories: Array
+})
+</script>
+
+<style  scoped>
+
+</style>
