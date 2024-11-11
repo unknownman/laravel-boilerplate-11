d75b6dfe3bc950d868942a6aa4e153d25bbd8a98
ali alavi <ali.masoudi.alavi@gmail.com>
2024-11-11 11:40:45 +0330

    adding notifable to category Model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Category extends Model
 {
+    use Notifiable;
     //
     protected $fillable = ['name', 'slug', 'parent_id', 'user_id'];
 
