5c0881be1b67352a3330e2228837e0def3ea3190
ali alavi <ali.masoudi.alavi@gmail.com>
2024-11-11 11:51:26 +0330

    config telegram bot api in service

--- config/services.php
+++ config/services.php
@@ -34,5 +34,8 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'telegram-bot-api' => [
+        'token' => env('TELEGRAM_BOT_TOKEN')
+    ]
 
 ];
