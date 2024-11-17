a67c0749c05d53087afad24bf7a445b7f179ccd9
ali alavi <ali.masoudi.alavi@gmail.com>
2024-11-17 18:24:22 +0330

    edit controller and vue to get all users except current login user to show them in vue

--- app/Http/Controllers/MessengerController.php
+++ app/Http/Controllers/MessengerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -9,6 +10,7 @@ class MessengerController extends Controller
 {
     //
     public function index() {
-        return Inertia::render("Messenger/Index");
+        $users = User::all()->whereNotIn("id", auth()->id());
+        return Inertia::render("Messenger/Index", compact("users"));
     }
 }
--- resources/js/Pages/Messenger/Index.vue
+++ resources/js/Pages/Messenger/Index.vue
@@ -5,6 +5,12 @@
 </template>
 
 <script setup>
+import { usePage } from '@inertiajs/vue3';
+
+const props = defineProps({
+    users: Array
+})
+const page = usePage()
 const echo = window.Echo
 echo.channel("chat").listenToAll((e,data) => {
     console.log("new event", e, data);
