50cb9be7c6fca5de2d364c38ba8d7b246f339692
ali alavi <ali.masoudi.alavi@gmail.com>
2024-11-17 18:54:49 +0330

    writing a function to handle on chat with a friend

--- resources/js/Components/Ui/Messenger/Friends.vue
+++ resources/js/Components/Ui/Messenger/Friends.vue
@@ -1,15 +1,25 @@
 <template>
-    <div>
-
-    </div>
+    <ul>
+        <li v-for="u,i in users" @click="onChatWith(u)">{{ u.name }}</li>
+    </ul>
 </template>
 
 <script setup>
 const props = defineProps({
     users: Array
 })
+const emit = defineEmits(["chat"])
+
+function onChatWith(u) {
+    emit("chat", u);
+}
 </script>
 
 <style scoped>
-
+ul {
+    @apply w-1/4 max-w-xs overflow-y-auto overflow-x-hidden bg-slate-200 h-screen
+}
+li {
+    @apply h-12 flex flex-col place-content-center place-items-start ps-5  cursor-pointer hover:bg-white hover:shadow-inner
+}
 </style>
