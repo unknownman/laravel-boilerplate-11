c0266d90a283fa28a25f45371067c2400b0180e2
ali alavi <ali.masoudi.alavi@gmail.com>
2024-11-17 19:36:58 +0330

    edit event to get chat model

--- app/Events/ChatMessage.php
+++ app/Events/ChatMessage.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\Chat;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -18,17 +19,12 @@ class ChatMessage implements ShouldBroadcast
     /**
      * Create a new event instance.
      */
-    public function __construct($message)
+    public function __construct(Chat $message)
     {
         //
         $this->message = $message;
     }
 
-    public function broadcastWith() {
-        return [
-            "message" => $this->message
-        ];
-    }
 
     /**
      * Get the channels the event should broadcast on.
@@ -38,7 +34,7 @@ class ChatMessage implements ShouldBroadcast
     public function broadcastOn(): array
     {
         return [
-            new Channel('chat'),
+            new PrivateChannel("chat.{$this->message->to}"),
         ];
     }
 }
