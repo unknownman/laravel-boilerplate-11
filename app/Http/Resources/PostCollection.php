32dbabe197c5de70087605d1356b293a8ee48644
ali alavi <ali.masoudi.alavi@gmail.com>
2024-12-13 19:47:57 +0330

    31 Resource and Collections

---
+++ app/Http/Resources/PostCollection.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class PostCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "data" => $this->collection->transform(function($post) {
+                return [
+                    "id" => $post->id,
+                    "title" => $post->title,
+                    "description" => $post->description,
+                    "publish_date" => jdate($post->created_at->timestamp)->format("Y-m-d H:i"),
+                    "author" => new UserResource($post->author)
+                ];
+            }),
+            'meta' => [
+                'current_page' => $this->currentPage(),
+                'total_pages' => $this->lastPage(),
+                'total_items' => $this->total()
+            ],
+            'links' => [
+                "self" => $this->path(),
+                "next" => $this->nextPageUrl(),
+                "prev" => $this->previousPageUrl(),
+            ]
+        ];
+    }
+}
